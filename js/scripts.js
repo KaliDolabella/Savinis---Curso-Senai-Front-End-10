08fa09b3d843f7f49a4408adae6f3decad831550
KaliDolabella <kalidolabella@gmail.com>
2022-11-22 20:15:10 -0300

    modificações na página principal, criação de pagina de estilo para login, criação de código javascript para login

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+function login(event){
+    let email = document.getElementById("email")
+    let senha = document.getElementById("senha")
+
+    console.log(email.value)
+    console.log(senha.value)
+}
+
