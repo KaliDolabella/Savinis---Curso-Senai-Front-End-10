dcdf162e770101c8f68a9b85a4ef4cedd41b116c
KaliDolabella <kalidolabella@gmail.com>
2022-11-23 19:09:42 -0300

    criação de funções js mostrar menu e cadastrar newsletter

--- js/scripts.js
+++ js/scripts.js
@@ -6,3 +6,21 @@ function login(event){
     console.log(senha.value)
 }
 
+function mostrarMenu () {
+    let menu = document.getElementById("menu")
+
+    if (getComputedStyle(menu).display == 'none') {
+        menu.style.display = 'flex';
+    } else {
+        menu.style.display = 'none';
+    }
+}
+
+
+function cadastrarNewsletter() {
+    let email = document.getElementById("campo-email").value
+
+    alert(email)
+    console.log(email)
+    
+}
